12a3dde8efbef43c0d4bef0b69319d1cdd870ed5
Davi Soria <davisoriamaia@gmail.com>
2025-03-20 21:12:00 -0300

    Incluido dinâmicas no projeto

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,22 +1,43 @@
 import {Request, Response} from "express";
+import { createMenuOptions } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
-
-
+    res.render("pages/page", {
+        menu: createMenuOptions('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 };
 
 export const dogs = (req: Request, res: Response) => {
-    
-
+    res.render("pages/page", {
+        menu: createMenuOptions('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 };
 
 export const cats = (req: Request, res: Response) => {
-    
-
+    res.render("pages/page", {
+        menu: createMenuOptions('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 };
 
 export const fishes = (req: Request, res: Response) => {
-    
-
+    res.render("pages/page", {
+        menu: createMenuOptions('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 };
 
