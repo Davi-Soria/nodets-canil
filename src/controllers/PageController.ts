08ccde42aaf190f3b8eece91e48d2f0b1d074e2c
Davi Soria <davisoriamaia@gmail.com>
2025-03-21 21:29:19 -0300

    Incluido os models e a ferramenta de busca

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,43 +1,56 @@
 import {Request, Response} from "express";
 import { createMenuOptions } from "../helpers/createMenuObject";
+import {pet} from "../models/pets";
 
 export const home = (req: Request, res: Response) => {
+    let list = pet.getAll();
+
     res.render("pages/page", {
         menu: createMenuOptions('all'),
         banner: {
             title: 'Todos os animais',
-            background: 'allanimals.jpg'
-        }
+            background: 'allanimals.jpg',
+        },
+        list
     });
 };
 
 export const dogs = (req: Request, res: Response) => {
+    let list = pet.getFromType('dog');
+
     res.render("pages/page", {
         menu: createMenuOptions('dog'),
         banner: {
             title: 'Cachorros',
-            background: 'banner_dog.jpg'
-        }
+            background: 'banner_dog.jpg',
+        },
+        list
     });
 };
 
 export const cats = (req: Request, res: Response) => {
+    let list = pet.getFromType('cat');
+
     res.render("pages/page", {
         menu: createMenuOptions('cat'),
         banner: {
             title: 'Gatos',
-            background: 'banner_cat.jpg'
-        }
+            background: 'banner_cat.jpg',
+        },
+        list
     });
 };
 
 export const fishes = (req: Request, res: Response) => {
+    let list = pet.getFromType('fish');
+
     res.render("pages/page", {
         menu: createMenuOptions('fish'),
         banner: {
             title: 'Peixes',
-            background: 'banner_fish.jpg'
-        }
+            background: 'banner_fish.jpg',
+        },
+        list
     });
 };
 
--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -1,6 +1,14 @@
 import {Request, Response} from "express";
+import {pet} from "../models/pets";
+import {createMenuOptions} from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
+    let query:string = req.query.q as string;
 
+    let list = pet.getFromName(query);
 
+    res.render("pages/page", {
+        menu: createMenuOptions(''),
+        list
+    });
 };
