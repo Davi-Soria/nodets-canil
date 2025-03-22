fbc2f98a36fcbe80d848eb49f8a5c8ddd36d06fe
Davi Soria <davisoriamaia@gmail.com>
2025-03-22 20:21:42 -0300

    final project

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -4,11 +4,16 @@ import {createMenuOptions} from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
     let query:string = req.query.q as string;
-
     let list = pet.getFromName(query);
+
+    if(!query){
+        res.redirect("/");
+        return
+    };
 
     res.render("pages/page", {
         menu: createMenuOptions(''),
-        list
+        list,
+        query
     });
 };
